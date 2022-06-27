0a460817d65b7404d2c398c8e42c8620514241f9
codyprashant <70243771+codyprashant@users.noreply.github.com>
2022-06-27 08:58:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ require('dotenv').config();
 require('./app/config/db');
 var cron = require('node-cron');
 const { vfsAppointmentChecker } = require('./app/controllers/vfsAppointmentChecker');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  const ENVIROINMENT = process.env.NODE_ENV || 'development';
+  const message = `Server Listening On Port ${PORT}, ENVIROINMENT=${ENVIROINMENT}`;
+  // eslint-disable-next-line no-console
+  console.log(message);
+});
 
 vfsAppointmentChecker();
 // cron.schedule('0 1-23 * * *', () => {
